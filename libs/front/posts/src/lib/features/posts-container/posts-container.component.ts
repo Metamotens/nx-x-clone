0d210737d7b07d24b449b849675cd35926c07cdd
Metamotens <roberhdezmora88@gmail.com>
2024-02-09 10:01:32 +0100

    tweet styles

--- libs/front/posts/src/lib/features/posts-container/posts-container.component.ts
+++ libs/front/posts/src/lib/features/posts-container/posts-container.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { PostsStore } from '../../data-access/posts.store';
 import { HeaderComponent } from '../../ui/header/header.component';
+import { PostComponent } from '../../ui/post/post.component';
+import { TweetFormComponent } from '../../ui/tweet-form/tweet-form.component';
 
 @Component({
   selector: 'x-clone-posts-container',
   standalone: true,
-  imports: [CommonModule, RouterLink, HeaderComponent],
+  imports: [CommonModule, RouterLink, HeaderComponent, PostComponent, TweetFormComponent],
   templateUrl: './posts-container.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
